af9a166a08093a5c91edb2feee3a55461f245298
asrafulashiq <asrafulashiq@gmail.com>
2021-04-25 23:59:08 -0400

    update

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,4 +1,4 @@
-launcher="local"
+launcher="slurm"
 while getopts "l:" opt; do
     case ${opt} in
     l)
@@ -14,7 +14,7 @@ for each_split in $(ls ./data_loading/splits/); do
     fi
 
     base_name=$(cut -d'.' -f1 <<<"${each_split}")
-    cmd="python main.py split_path=data_loading/splits/${each_split} \
+    cmd="python main.py mode=train split_path=data_loading/splits/${each_split} \
         coco_json_prefix=${each_split} name=kp_${base_name} ${LAUNCHER}"
     echo $cmd
     eval $cmd
