0cd8811e93490d491b5f544f8f7f9d4fbfc39d2a
asrafulashiq <asrafulashiq@gmail.com>
2021-05-01 17:00:54 -0400

    update

--- scripts/create_data.sh
+++ scripts/create_data.sh
@@ -1,6 +1,6 @@
-cd data_loading
-python create_splits.py
-cd ..
+# cd data_loading
+# python create_splits.py
+# cd ..
 
 launcher="local"
 while getopts "l:" opt; do
@@ -11,15 +11,23 @@ while getopts "l:" opt; do
     esac
 done
 
-for each_split in $(ls ./data_loading/splits/); do
+# for each_split in $(ls ./data_loading/splits/); do
 
-    if [ $launcher = "slurm" ]; then
-        LAUNCHER="launcher=slurm ngpus=1 timeout=01:00:00 mem_per_cpu=20000 cpus_per_task=2"
-    fi
+#     if [ $launcher = "slurm" ]; then
+#         LAUNCHER="launcher=slurm ngpus=1 timeout=01:00:00 mem_per_cpu=20000 cpus_per_task=2"
+#     fi
 
-    cmd="python main.py create_coco=true split_path=data_loading/splits/${each_split} \
-        coco_json_prefix=${each_split} name=data_${each_split} ${LAUNCHER}"
-    echo $cmd
-    eval $cmd
+#     cmd="python main.py create_coco=true split_path=data_loading/splits/${each_split} \
+#         coco_json_prefix=${each_split} name=data_${each_split} ${LAUNCHER}"
+#     echo $cmd
+#     eval $cmd
 
-done
+# done
+
+if [ $launcher = "slurm" ]; then
+    LAUNCHER="launcher=slurm ngpus=1 timeout=01:20:00 mem_per_cpu=20000 cpus_per_task=2"
+fi
+
+cmd="python main.py create_coco=true  name=data_${each_split} ${LAUNCHER}"
+echo $cmd
+eval $cmd
