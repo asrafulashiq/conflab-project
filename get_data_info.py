3c781ab9af729dffed68c5a329d8a318e111c254
asrafulashiq <asrafulashiq@gmail.com>
2021-06-18 09:05:12 -0400

    update

--- get_data_info.py
+++ get_data_info.py
@@ -28,9 +28,13 @@ for cam in cam_keys:
         tot_per += _data['total_person']
         tot_pt += _data['total_pt']
         null_pt += _data['null_pt']
+    tot_im = len(cam_keys[cam])
+
     cam_info[cam]['person'] = tot_per
     cam_info[cam]['keypoint'] = tot_pt
     cam_info[cam]['null'] = null_pt
+    cam_info[cam]['im'] = tot_im
+
     print(f"Camera {cam}")
     print("*" * 20)
 
@@ -38,8 +42,9 @@ for cam in cam_keys:
     print(f"\tKP: {tot_pt}")
     print(f"\tNull: {null_pt / tot_pt: .4f}")
 
+total_im = sum([v['im'] for v in cam_info.values()])
 total_per = sum([v['person'] for v in cam_info.values()])
 total_kp = sum([v['keypoint'] for v in cam_info.values()])
 total_null = sum([v['null'] for v in cam_info.values()])
 
-print(total_per, total_kp, total_null)
+print(total_im, total_per, total_kp, total_null)
